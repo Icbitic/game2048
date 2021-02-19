372b1b9bc0ad6ba20fb05fe022ddf1cf2ac78e35
Icbitic <Icbitic@foxmail.com>
2021-02-19 18:32:37 +0800

    fix bug

--- map.gd
+++ map.gd
@@ -17,14 +17,16 @@ func _ready():
 	place()
 	
 func _on_Game2048_slip(direction):
+	var merged_cell = []
 	for i in range(0, 4):
 		for j in range(0, 4):
 			for x in range(1,4):
 				var checked_pos = Vector2(i, j) + direction * x
 				if checked_pos.x in range(0, 4) and checked_pos.y in range(0, 4):
-					if get_cellv(checked_pos) == get_cell(i, j) and get_cellv(checked_pos) != -1:
+					if get_cellv(checked_pos) == get_cell(i, j) and get_cellv(checked_pos) != -1 and !merged_cell.has(Vector2(i, j)):
 						set_cellv(checked_pos, get_cellv(checked_pos) + 1)
 						set_cell(i, j, -1)
+						merged_cell.append(checked_pos)
 						emit_signal("merged", pow(2, get_cellv(checked_pos) + 1))
 						break
 	
